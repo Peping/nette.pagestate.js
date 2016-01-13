2f46a1ad37d96736d0500ed8ad950028bc71576b
Peping <Peping110@yahoo.com>
2016-01-13 20:20:30 +0100

    working version

--- nette.pagestate.js
+++ nette.pagestate.js
@@ -1,19 +1,22 @@
-$.nette.ext('pagestate',{
-    before: function (xhr, settings) {
-        xhr.setRequestHeader('X-NETTE-PAGESTATE',window.sessionStorage['nette-pagestate']);
-        return true;
-    }
-});
-
 (function() {
+    var pageState = '';
+
     var refreshPagestate = function () {
         var $ps = $('#nette-pagestate-container');
         if (!$ps.length) {
             return;
         }
 
-        window.sessionStorage['nette-pagestate'] = $ps.attr('data-state');
+        pageState = $ps.attr('data-state');
     };
+
+    $.nette.ext('pagestate',{
+        before: function (xhr, settings) {
+            xhr.setRequestHeader('X-Nette-Pagestate',pageState);
+            return true;
+        }
+    });
+
 
     $.nette.ext('snippets').after(function ($el) {
         refreshPagestate();
